62b75bd18bdc01e8c22ae1aaccdfb2c7528a0b9e
Ian Cullinane <ian.cullinane@gmail.com>
2022-05-05 14:38:27 -0400

    add removal policy option

--- lib/s3-bucket/index.ts
+++ lib/s3-bucket/index.ts
@@ -1,10 +1,11 @@
 
-import { CfnOutput, ITaggable, TagManager, Tags, Stack } from "aws-cdk-lib";
+import { ITaggable, TagManager, RemovalPolicy } from "aws-cdk-lib";
 import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
 
 export interface S3BucketProps {
   bucketName: string;
+  destroyOnDelete?: string;
 }
 
 export class AllPurposeBucket extends Construct implements ITaggable {
@@ -17,7 +18,12 @@ export class AllPurposeBucket extends Construct implements ITaggable {
 
     this.bucket = new s3.Bucket(this, `bucket-${props.bucketName}`, {
       bucketName: props.bucketName,
-      autoDeleteObjects: true,
     });
+
+    (props.destroyOnDelete === "yes"
+      ? this.bucket.applyRemovalPolicy(RemovalPolicy.DESTROY)
+      : null);
+
+
   }
 }
